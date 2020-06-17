4b1c4976142fb472f30b9e0db9a833618b19dd42
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-18 05:42:58 +0900

    [update]コンストラクタの定義
    コンストラクタはクラス名と同じ名前を持った特殊なメソッド。
    setのアクセスレベルをprivateにすることでProductクラスの利用者が
    コンストラクタ以外でプロパティの値を変更できなくなる。

--- HelloWorld/chapter1/Product.cs
+++ HelloWorld/chapter1/Product.cs
@@ -1,10 +1,17 @@
 // 商品クラス
 public class Product
 {
-    public int Code { get; set; } // 商品コード
-    public string Name { get; set; } // 商品名
-    public int Price { get; set; } // 商品価格
+    public int Code { get; private set; } // 商品コード
+    public string Name { get; private set; } // 商品名
+    public int Price { get; private set; } // 商品価格
 
+    // コンストラクタ
+    public Product(int code, string name, int price)
+    {
+        this.Code = code;
+        this.Name = name;
+        this.Price = price;
+    }
     // 消費税額を求める（消費税率は8%）
     public int GetTax()
     {
