eb10b9cd966843bf53c8ab5bda0f2b0a80616a41
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-18 06:36:06 +0900

    [add]AtCoder ABC088 A

---
+++ procon/agc088_a/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace agc088_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long a = long.Parse(Console.ReadLine());
+            Console.WriteLine(n % 500 <= a ? "Yes" : "No");
+        }
+    }
+}
