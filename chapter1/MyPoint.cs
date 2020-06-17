097e0834ba6bf4e4b7adef69952603391635e78b
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-18 06:44:52 +0900

    [clean]不要なファイルを削除、フォルダ構成の見直し、エラーの修正
    ・Hello Worldプロジェクトを削除
    ・学習用の書籍フォルダとAtCoder用のフォルダに分割
    ・using Systemにセミコロンを付け忘れていたエラーを解消

---
+++ chapter1/MyPoint.cs
@@ -0,0 +1,29 @@
+using System;
+struct MyPoint
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public MyPoint(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+}
+
+class Test
+{
+    public void Main(string[] args)
+    {
+        MyPoint a = new MyPoint(10, 20);
+        MyPoint b = a;
+
+        Console.WriteLine("a: ({0}, {1})", a.X, a.Y); // a: (10, 20)
+        Console.WriteLine("b: ({0}, {1})", b.X, b.Y); // b: (10, 20)
+
+        a.X = 80;
+
+        Console.WriteLine("a: ({0}, {1})", a.X, a.Y); // a: (80, 20)
+        Console.WriteLine("b: ({0}, {1})", b.X, b.Y); // b: (10, 20)
+    }
+}
