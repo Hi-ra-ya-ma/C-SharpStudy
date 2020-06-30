051546a91c7005db63371df77d47767e920c9259
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-30 21:52:56 +0900

    [update]第1章の演習問題 問題2

--- book/chapter1/Inheritance.cs
+++ book/chapter1/Inheritance.cs
@@ -18,6 +18,12 @@ public class Employee : Person
     public string DivisionName { get; set; }
 }
 
+public class Student : Person
+{
+    public int Grade { get; set; }
+    public int ClassNumber { get; set; }
+}
+
 class Test
 {
     public void Main(string[] arg)
@@ -31,5 +37,18 @@ class Test
         };
 
         Console.writeLine("{0}({1})は、{2}に所属しています。", employee.Name, employee.GetAge(), employee.DivisionName);
+
+        var student = new Student
+        {
+            Name = "tanaka",
+            Birthday = new DateTime(1997, 1, 29),
+            Grade = 3,
+            ClassNumber = 1,
+        };
+
+        System.Console.WriteLine("{0}年{1}組の{2}です。誕生日は{3}です。", student.Grade, student.ClassNumber, student.Name, student.Birthday);
+
+        Person hoge = student;
+        object fuga = student;
     }
 }
