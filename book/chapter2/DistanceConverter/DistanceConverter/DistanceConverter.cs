ec449a8c2fef28f6a247b66c34c0051123046619
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-07-05 15:43:15 +0900

    [update]Chapter2 プログラムに機能を追加する
    与えられた引数で処理を分岐するよう機能追加

---
+++ book/chapter2/DistanceConverter/DistanceConverter/DistanceConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DistanceConverter
+{
+    public class DistanceConverter
+    {
+        public DistanceConverter()
+        {
+        }
+
+        public double FeetToMeter(int feet)
+        {
+            return feet * 0.3048;
+        }
+
+        public double MeterToFeet(int meter)
+        {
+            return meter / 0.3048;
+        }
+    }
+}
--- book/chapter2/DistanceConverter/DistanceConverter/Program.cs
+++ book/chapter2/DistanceConverter/DistanceConverter/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace DistanceConverter
 {
@@ -6,7 +6,18 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var distanceConverter = new DistanceConverter();
+            var maxNum = 10;
+            var converterFlg = args.Length >= 1 && args[0] == "-tom";
+            foreach (var i in Enumerable.Range(1, maxNum))
+            {
+                System.Console.WriteLine
+                (
+                    "{0} ft = {1: 0.0000} m",
+                    i,
+                    converterFlg ? distanceConverter.FeetToMeter(i) : distanceConverter.MeterToFeet(i)
+                );
+            }
         }
     }
 }
