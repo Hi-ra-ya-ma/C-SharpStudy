ad2e83ace7cdbdd2fd82b6dcdf0bd2bb2746f59b
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-07-06 22:31:42 +0900

    [add]Chapter2 CSVファイルを読み込むプログラムを新規作成

---
+++ book/chapter2/SalesSummaryProgram/SalesSummaryProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SalesSummaryProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // 売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string filePath)
+        {
+            var sales = new List<Sale>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                var items = line.Split(',');
+                var sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
+            }
+
+            return sales;
+        }
+    }
+}
---
+++ book/chapter2/SalesSummaryProgram/SalesSummaryProgram/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SalesSummaryProgram
+{
+    public class Sale
+    {
+        // 店舗名
+        public string ShopName { get; set; }
+
+        // 商品カテゴリ
+        public string ProductCategory { get; set; }
+
+        // 売上高
+        public int Amount { get; set; }
+    }
+}
